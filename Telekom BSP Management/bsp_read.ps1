b91cf669f1bb780a9d8d566f4390a63cf32e7260
Martin <111351643+MortysTerminal@users.noreply.github.com>
2022-09-26 16:21:46 +0200

    Aufräumarbeiten

---
+++ Telekom BSP Management/bsp_read.ps1
@@ -0,0 +1,33 @@
+<#
+.DESCRIPTION
+  Um eine Verbindung mit dem BSP_Portal von Telekom herzustellen und diverse Anfragen zu stellen, wurde dieses Skript erstellt.
+  Es ließt die aktuellen Vertraege aus und die aktuellen Preise von iPhone SE - 14 Pro (alle Speichergroessen)
+  TODO: Alternativ kann eine SQL Anbindung geschaffen werden.
+  
+.INPUTS
+  Keine
+
+.OUTPUTS
+  Tabelle mit den Vertraegen und Preisen
+
+.NOTES
+  Version:        0.1
+  Author:         Martin 
+  Creation Date:  26.09.2022
+#>
+
+############################
+### INIT  - Benoetigte Variabelen
+############################
+
+############################
+### Funktionen
+############################
+
+
+
+############################
+### START
+############################
+
+
